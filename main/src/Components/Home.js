062540bbb189fe36e5e490725dfb2668a05a693a
Bishal Poudel <poudelb172@gmail.com>
2021-12-12 19:51:19 +0545

    Enter component made

--- main/src/Components/Home.js
+++ main/src/Components/Home.js
@@ -2,9 +2,17 @@ import React from 'react'
 import './Home.css'
 
 export default function Home() {
+
     return (
         <>
-            Lets create a todo app
+            <div className="Container">
+                <div className="inputContainer">
+                    <input type="text" placeHolder="Enter into todo-list !!!"
+                        className="entertodo" name="" id="" />
+                    <input className="btn" type="button" value="ADD" />
+                </div>
+
+            </div>
         </>
     )
 }

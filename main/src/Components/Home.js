0d191dfed7fd4cd6b41086b2ff17878715d09d86
Bishal Poudel <poudelb172@gmail.com>
2021-12-12 20:51:59 +0545

    message component made

--- main/src/Components/Home.js
+++ main/src/Components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Home.css'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 export default function Home() {
 
@@ -10,6 +12,38 @@ export default function Home() {
                     <input type="text" placeHolder="Enter into todo-list !!!"
                         className="entertodo" name="" id="" />
                     <input className="btn" type="button" value="ADD" />
+                </div>
+
+                <div className="lists">
+                    <div className="listContainer">
+                        <div className="message">
+                            My first list
+                        </div>
+                        <div className="iconsContainer">
+                            <EditIcon className="icons" style={{ color: "teal" }} />
+                            <DeleteOutlinedIcon className="icons" style={{ color: "tomato" }} />
+                        </div>
+                    </div>
+
+                    <div className="listContainer">
+                        <div className="message">
+                            My first list
+                        </div>
+                        <div className="iconsContainer">
+                            <EditIcon className="icons" style={{ color: "teal" }} />
+                            <DeleteOutlinedIcon className="icons" style={{ color: "tomato" }} />
+                        </div>
+                    </div>
+
+                    <div className="listContainer">
+                        <div className="message">
+                            My first list
+                        </div>
+                        <div className="iconsContainer">
+                            <EditIcon className="icons" style={{ color: "teal" }} />
+                            <DeleteOutlinedIcon className="icons" style={{ color: "tomato" }} />
+                        </div>
+                    </div>
                 </div>
 
             </div>

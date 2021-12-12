657f34d8a9799b5e6f444ac55caaf0ff04b84a9b
Bishal Poudel <poudelb172@gmail.com>
2021-12-12 21:36:03 +0545

    done by useState hook

---
+++ main/src/Components/Data.js
@@ -0,0 +1,6 @@
+export const data = [
+    {
+        id: "1639325838979",
+        task: "lets go"
+    }
+]
--- main/src/Components/Home.js
+++ main/src/Components/Home.js
@@ -1,49 +1,60 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 import './Home.css'
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import { data } from './Data';
 
 export default function Home() {
 
+    const [marray, setArray] = useState(data)
+    const [task, setTask] = useState("")
+
+    //By Using UseReducer Hook
+    const initialState=[{
+        uvale:data
+    }]
+
+    const reducer=(state,action)=>{
+
+    }
+    const [state, dispatch] = useReducer(reducer, initialState)
+    //if done normally by useState 
+    // const addTask = () => {
+    //     setArray([...marray,
+    //     {
+    //         id: new Date().getTime().toString(),
+    //         task
+    //     }])
+    // }
+
+    // const handleDelete = (id) => {
+    //  setArray(marray.filter((item) => item.id !== id))
+    // }
     return (
         <>
             <div className="Container">
                 <div className="inputContainer">
-                    <input type="text" placeHolder="Enter into todo-list !!!"
+                    <input value={task} onChange={e => setTask(e.target.value)}
+                        type="text" placeHolder="Enter into todo-list !!!"
                         className="entertodo" name="" id="" />
-                    <input className="btn" type="button" value="ADD" />
+                    <input onClick={addTask} className="btn" type="button" value="ADD" />
                 </div>
 
                 <div className="lists">
-                    <div className="listContainer">
-                        <div className="message">
-                            My first list
-                        </div>
-                        <div className="iconsContainer">
-                            <EditIcon className="icons" style={{ color: "teal" }} />
-                            <DeleteOutlinedIcon className="icons" style={{ color: "tomato" }} />
-                        </div>
-                    </div>
-
-                    <div className="listContainer">
-                        <div className="message">
-                            My first list
-                        </div>
-                        <div className="iconsContainer">
-                            <EditIcon className="icons" style={{ color: "teal" }} />
-                            <DeleteOutlinedIcon className="icons" style={{ color: "tomato" }} />
-                        </div>
-                    </div>
-
-                    <div className="listContainer">
-                        <div className="message">
-                            My first list
-                        </div>
-                        <div className="iconsContainer">
-                            <EditIcon className="icons" style={{ color: "teal" }} />
-                            <DeleteOutlinedIcon className="icons" style={{ color: "tomato" }} />
-                        </div>
-                    </div>
+                    {
+                        marray.map((x) => (
+                            <div className="listContainer">
+                                <div className="message">
+                                    {x.task}
+                                </div>
+                                <div className="iconsContainer">
+                                    <EditIcon className="icons" style={{ color: "teal" }} />
+                                    <DeleteOutlinedIcon onClick={() => handleDelete(x.id)}
+                                        className="icons" style={{ color: "tomato" }} />
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
 
             </div>
